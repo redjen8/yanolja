686d4fe4c0d02c453e9c8efbcc1cb6c3f166773d
redjen8 <seokwoo2525@gmail.com>
2021-11-04 22:56:58 +0900

    added swagger specific statement, modified some api controllers to not use paramMap

--- src/main/java/com/redjen/yanolja/controller/ApiController.java
+++ src/main/java/com/redjen/yanolja/controller/ApiController.java
@@ -36,8 +36,7 @@ public class ApiController {
 
     @GetMapping("/member/get")
     @ApiOperation(value="사용자 정보 조회", notes="해당 인덱스의 사용자 정보를 조회한다.")
-    public ResponseEntity<Map<String, Object>> getMemberByIdx(@RequestParam HashMap<String, String> paramMap) {
-        int idx = Integer.parseInt(paramMap.get("memberIdx"));
+    public ResponseEntity<Map<String, Object>> getMemberByIdx(@RequestParam int idx) {
 
         Member member = memberService.searchMemberByIdx(idx);
         Map<String, Object> resultMap = new HashMap<>();
@@ -48,11 +47,8 @@ public class ApiController {
     }
 
     @GetMapping("/room/search/available")
-    @ApiOperation(value="예약 가능한 숙소 조회", notes="전체 방 중 현재 예약이 가능한 방 목록을 조회한다.")
-    public ResponseEntity<Map<String, Object>> searchAvailableRoomList(@RequestParam HashMap<String, String> paramMap) {
-        String conditionStart = paramMap.get("conditionStart");
-        String conditionEnd = paramMap.get("conditionEnd");
-
+    @ApiOperation(value="예약 가능한 숙소 조회", notes="전체 방 중 conditionStart ~ conditionEnd까지 예약이 가능한 방 목록을 조회한다.")
+    public ResponseEntity<Map<String, Object>> searchAvailableRoomList(@RequestParam String conditionStart, @RequestParam String conditionEnd) {
         List<Room> availRoomList = roomService.searchAvailableRoomList(conditionStart, conditionEnd);
         Map<String, Object> resultMap = new HashMap<>();
         resultMap.put("data", availRoomList);
@@ -60,12 +56,8 @@ public class ApiController {
     }
 
     @GetMapping("/room/search/coupon-available")
-    @ApiOperation(value="쿠폰 사용 가능하고 예약 가능한 숙소 조회", notes="쿠폰 사용 가능한 방 중 현재 예약이 가능한 방 목록을 조회한다.")
-    public ResponseEntity<Map<String, Object>> searchCouponAvailableRoomList(@RequestParam HashMap<String, String> paramMap) {
-        String conditionStart = paramMap.get("conditionStart");
-        String conditionEnd = paramMap.get("conditionEnd");
-        int couponIdx = Integer.parseInt(paramMap.get("couponIdx"));
-
+    @ApiOperation(value="쿠폰 사용 가능하고 예약 가능한 숙소 조회", notes="couponIdx를 가지는 쿠폰을 사용 가능한 방 중 conditionStart ~ conditionEnd까지 예약이 가능한 방 목록을 조회한다.")
+    public ResponseEntity<Map<String, Object>> searchCouponAvailableRoomList(@RequestParam String conditionStart, @RequestParam String conditionEnd, @RequestParam int couponIdx) {
         List<Room> availRoomList = roomService.searchCouponAvailableRoomList(conditionStart, conditionEnd, couponIdx);
         Map<String, Object> resultMap = new HashMap<>();
         resultMap.put("data", availRoomList);
@@ -119,8 +111,7 @@ public class ApiController {
 
     @GetMapping("/review/search")
     @ApiOperation(value="숙소 리뷰 목록 조회", notes="숙소의 모든 리뷰 목록을 조회한다.")
-    public ResponseEntity<Map<String, Object>> searchReviewList(@RequestParam HashMap<String, String> paramMap) {
-        int companyIdx = Integer.parseInt(paramMap.get("companyIdx"));
+    public ResponseEntity<Map<String, Object>> searchReviewList(@RequestParam int companyIdx) {
         Map<String, Object> resultMap = new HashMap<>();
         List<Review> reviewList = reviewService.searchReviewList(companyIdx);
         resultMap.put("data", reviewList);
@@ -129,8 +120,7 @@ public class ApiController {
 
     @GetMapping("/company/list/by-reserve")
     @ApiOperation(value="예약 내림차 순 카테고리 별 숙소 조회", notes="예약 수의 내림차순으로 숙소 목록을 조회한다.")
-    public ResponseEntity<Map<String, Object>> searchCompanyByReserve(@RequestParam HashMap<String, String> paramMap) {
-        int listSize = Integer.parseInt(paramMap.get("listSize"));
+    public ResponseEntity<Map<String, Object>> searchCompanyByReserve(@RequestParam int listSize) {
         Map<String, Object> resultMap = new HashMap<>();
         List<Company> companyList = companyService.searchCompanyByReserve(listSize);
         resultMap.put("data", companyList);
@@ -139,8 +129,7 @@ public class ApiController {
 
     @GetMapping("/company/list/by-likes")
     @ApiOperation(value="좋아요 내림차 순 카테고리 별 숙소 조회", notes="좋아요 수의 내림차순으로 숙소 목록을 조회한다.")
-    public ResponseEntity<Map<String, Object>> searchCompanyByLikes(@RequestParam HashMap<String, String> paramMap) {
-        int listSize = Integer.parseInt(paramMap.get("listSize"));
+    public ResponseEntity<Map<String, Object>> searchCompanyByLikes(@RequestParam int listSize) {
         Map<String, Object> resultMap = new HashMap<>();
         List<Company> companyList = companyService.searchCompanyByLikes(listSize);
         resultMap.put("data", companyList);
@@ -149,8 +138,7 @@ public class ApiController {
 
     @GetMapping("/company/list/by-location")
     @ApiOperation(value="지역 별 숙소 조회", notes="도/ 시,군,구 정보와 일치하는 숙소 목록을 조회한다.")
-    public ResponseEntity<Map<String, Object>> searchCompanyByLocation(@RequestParam HashMap<String, String> paramMap) {
-        String searchCondition = paramMap.get("searchCondition");
+    public ResponseEntity<Map<String, Object>> searchCompanyByLocation(@RequestParam String searchCondition) {
         Map<String, Object> resultMap = new HashMap<>();
         List<Company> companyList = companyService.searchCompanyByLocation(searchCondition);
         resultMap.put("data", companyList);
@@ -159,8 +147,7 @@ public class ApiController {
 
     @GetMapping("/owner/list")
     @ApiOperation(value="사업자 목록 조회", notes="숙소 사업자 대표의 정보를 조회한다.")
-    public ResponseEntity<Map<String, Object>> searchCompanyOwner(@RequestParam HashMap<String, String> paramMap) {
-        int companyIdx = Integer.parseInt(paramMap.get("companyIdx"));
+    public ResponseEntity<Map<String, Object>> searchCompanyOwner(@RequestParam int companyIdx) {
         Map<String, Object> resultMap = new HashMap<>();
         List<CompanyOwner> companyOwnerList = companyOwnerService.searchCompanyOwner(companyIdx);
         resultMap.put("data", companyOwnerList);
