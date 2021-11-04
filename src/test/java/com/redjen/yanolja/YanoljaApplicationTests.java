08e51da539b4dad4011bc78c417223c33561b20d
redjen8 <seokwoo2525@gmail.com>
2021-11-04 20:50:20 +0900

    added missing YanoljaApplication

---
+++ src/test/java/com/redjen/yanolja/YanoljaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redjen.yanolja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanoljaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
