1d364dd77574c0b0e8950505f07c242721691cb9
redjen8 <seokwoo2525@gmail.com>
2021-11-16 21:09:04 +0900

    added jwt services

---
+++ src/main/java/com/redjen/yanolja/security/Authority.java
@@ -0,0 +1,5 @@
+package com.redjen.yanolja.security;
+
+public enum Authority {
+    ROLE_USER, ROLE_ADMIN
+}
---
+++ src/main/java/com/redjen/yanolja/security/JwtService.java
@@ -0,0 +1,56 @@
+package com.redjen.yanolja.security;
+
+import com.redjen.yanolja.configuration.BaseException;
+import com.redjen.yanolja.configuration.BaseResponseStatus;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    public String createJwt(int memberIdx) {
+        Date now = new Date();
+        return Jwts.builder()
+                .setHeaderParam("type", "jwt")
+                .claim("memberIdx", memberIdx)
+                .setIssuedAt(now)
+                .setExpiration(new Date(System.currentTimeMillis() + (1000 * 60 * 60 * 24)))
+                .signWith(SignatureAlgorithm.HS256, Secret.JWT_SECRET_KEY)
+                .compact();
+    }
+
+    public String getJwt() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        return request.getHeader("X-ACCESS-TOKEN");
+    }
+
+    public int getUserIdx() throws BaseException{
+        //1. JWT 추출
+        String accessToken = getJwt();
+        if(accessToken == null || accessToken.length() == 0){
+            throw new BaseException(BaseResponseStatus.EMPTY_JWT);
+        }
+
+        // 2. JWT parsing
+        Jws<Claims> claims;
+        try{
+            claims = Jwts.parser()
+                    .setSigningKey(Secret.JWT_SECRET_KEY)
+                    .parseClaimsJws(accessToken);
+        } catch (Exception ignored) {
+            System.out.println("invalid jwt");
+            throw new BaseException(BaseResponseStatus.INVALID_JWT);
+        }
+
+        // 3. userIdx 추출
+        return claims.getBody().get("userIdx",Integer.class);  // jwt 에서 userIdx를 추출합니다.
+    }
+}
