dd23e0e086d046376071fdfd70d9a9f3d2b0bc54
redjen8 <seokwoo2525@gmail.com>
2021-12-01 23:12:19 +0900

    result list up with b-table

--- yanolja-front/src/views/RoomList.vue
+++ yanolja-front/src/views/RoomList.vue
@@ -3,51 +3,64 @@
     <b-row>
       <h1>RoomList</h1>
     </b-row>
-    <b-row align-v="center">
-      <b-col sm="4">
-        예약 시작 시간 : {{ startDate }} {{ startTime }}
-      </b-col>
-      <b-col sm="4">
-        <b-calendar id="calendarStartDate" v-model="startDate" selected-variant="primary"></b-calendar>
-      </b-col>
-      <b-col sm="4">
-        <b-time id="timePickerStartTime" v-model="startTime"></b-time>
-      </b-col>
-    </b-row>
-    <hr>
-    <b-row align-v="center">
-      <b-col sm="4">
-        예약 종료 시간 : {{ endDate }} {{ endTime }}
-      </b-col>
-      <b-col sm="4">
-        <b-calendar id="calendarEndDate" v-model="endDate" selected-variant="primary"></b-calendar>
-      </b-col>
-      <b-col sm="4">
-        <b-time id="timePickerEndTime" v-model="endTime"></b-time>
-      </b-col>
-    </b-row>
-
-    <br>
-    <b-button id="btnSelectConditionRoom" @click="getRoomListAvailable()">조회</b-button>
+    <div v-if="!bSearched">
+      <b-row align-v="center">
+        <b-col sm="4">
+          예약 시작 시간 : {{ startDate }} {{ startTime }}
+        </b-col>
+        <b-col sm="4">
+          <b-calendar id="calendarStartDate" v-model="startDate" selected-variant="primary" hide-header></b-calendar>
+        </b-col>
+        <b-col sm="4">
+          <b-time id="timePickerStartTime" v-model="startTime"></b-time>
+        </b-col>
+      </b-row>
+      <hr>
+      <b-row align-v="center">
+        <b-col sm="4">
+          예약 종료 시간 : {{ endDate }} {{ endTime }}
+        </b-col>
+        <b-col sm="4">
+          <b-calendar id="calendarEndDate" v-model="endDate" selected-variant="primary" hide-header></b-calendar>
+        </b-col>
+        <b-col sm="4">
+          <b-time id="timePickerEndTime" v-model="endTime"></b-time>
+        </b-col>
+      </b-row>
+      <br>
+      <b-button id="btnSelectConditionRoom" @click="getRoomListAvailable()">조회</b-button>
+    </div>
+    <div v-if="bSearched">
+      <b-table :items="roomSearchResult" :fields="tableFields" :head-variant="tableHeadVariant"></b-table>
+    </div>
   </b-container>
 </template>
 
 <script>
 import axios from 'axios'
 
+function getTodayDate() {
+  return now.getFullYear() + "-" + ("00" + (now.getMonth() + 1)).slice(-2) + "-" + ("00" + now.getDate()).slice(-2)
+}
+
 let now = new Date();
-const todayDate = now.getFullYear() + "-" + ("00" + (now.getMonth() + 1)).slice(-2) + "-" + ("00" + now.getDate()).slice(-2)
+const todayDate = getTodayDate()
 now.setDate(now.getDate() + 1)
-const tomorrowDate = now.getFullYear() + "-" + ("00" + (now.getMonth() + 1)).slice(-2) + "-" + ("00" + now.getDate()).slice(-2)
+const tomorrowDate = getTodayDate()
+
 
 export default {
   name: "RoomList",
   data: () => {
     return {
+      bSearched: false,
+      roomSearchResult: null,
       startDate: todayDate,
       startTime: '18:00:00',
       endDate: tomorrowDate,
       endTime: '10:00:00',
+      tableHeadVariant: 'light',
+      tableFields: ['roomName', 'roomShortInfo', 'roomRemainCnt', 'maxPersonCnt', 'defaultPersonCnt', 'roomDescription', 'roomCategory', 'roomPrice', 'bSookbak', 'bDaesil'],
     }
   },
   methods: {
@@ -64,12 +77,16 @@ export default {
           // }
         })
         .then(res => {
-          alert(JSON.stringify(res.data))
+          this.roomSearchResult = res.data.data
+          this.toggleSearched()
         })
         .catch(err => {
           console.log(err)
         })
     },
+    toggleSearched() {
+      this.bSearched = !this.bSearched
+    }
   }
 }
 </script>
