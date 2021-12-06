3fd34bfca39dad60a1ff3e748a6b10aa4fbb37a6
redjen8 <seokwoo2525@gmail.com>
2021-12-06 22:42:49 +0900

    added b-navbar on top

--- yanolja-front/src/App.vue
+++ yanolja-front/src/App.vue
@@ -1,26 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <b-container fluid>
-        <b-row>
-          <b-col sm="2" align="start">
-            <router-link to="/">Home</router-link>
-          </b-col>
-          <b-col sm="3">
-            <router-link to="/about">About</router-link>
-          </b-col>
-          <b-col sm="3">
-            <router-link to="/room">Room</router-link>
-          </b-col>
-          <b-col sm="2" align="end">
-            <router-link to="/member">Member</router-link>
-          </b-col>
-          <b-col sm="2" align="end">
-            <router-link to="/login">Login</router-link>
-          </b-col>
-        </b-row>
-      </b-container>
-    </div>
+    <Header/>
     <router-view/>
   </div>
 </template>
@@ -34,20 +14,10 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-  background-color: #000000;
-  color: #FFFFFF;
-  margin-bottom: 20px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #FFFFFF;
-  text-decoration: none;
-}
-
-/*#nav a.router-link-exact-active {*/
-/*  color: #42b983;*/
-/*}*/
 </style>
+<script>
+import Header from "@/views/Header";
+export default {
+  components: {Header}
+}
+</script>
---
+++ yanolja-front/src/views/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="navbar">
+    <b-navbar toggleable="lg" class="nav" type="dark" variant="primary" sticky>
+      <b-navbar-brand class="navbar-brand" href="/">Yanolja</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-main-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-main-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/about">About</b-nav-item>
+          <b-nav-item to="/room">Room</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ms-auto">
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              User
+            </template>
+            <b-dropdown-item to="/login">Login</b-dropdown-item>
+            <b-dropdown-item to="/member">Sign In</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+#navbar {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;*/
+  margin-bottom: 20px;
+  color: #2c3e50;
+}
+
+.navbar-brand {
+  margin-left: 15px;
+  font-family: "Arial Black",serif;
+}
+
+</style>
