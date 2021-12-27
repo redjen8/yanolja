24e478add752b1c197620d010c081df6c328a53e
redjen8 <seokwoo2525@gmail.com>
2021-12-27 21:09:07 +0900

    added authority enum role

--- src/main/java/com/redjen/yanolja/security/Authority.java
+++ src/main/java/com/redjen/yanolja/security/Authority.java
@@ -1,5 +1,16 @@
 package com.redjen.yanolja.security;
 
+import lombok.Getter;
+
+@Getter
 public enum Authority {
-    ROLE_USER, ROLE_ADMIN
+    ROLE_USER("사용자", "일반 사용자 권한"), ROLE_ADMIN("사업자", "사업주 계정 권한");
+
+    private final String role;
+    private final String description;
+
+    Authority(String role, String description) {
+        this.role = role;
+        this.description = description;
+    }
 }
